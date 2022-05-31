3f8d97ae7cf5df3ce1100151e5f68c731d9ab12a
Thomas Spader <tspader@vmware.com>
2022-05-31 17:37:24 -0400

    mac build

--- src/platform.cpp
+++ src/platform.cpp
@@ -16,11 +16,6 @@
 #define _ad_train_executable   _ad_install      "/" "ad_train"     _ad_executable_ext
 #define _ad_install            _ad_build        "/" "bin"
 
-#ifdef _WIN32
-#include <windows.h>
-
-#define _ad_executable_ext ".exe"
-
 namespace paths {
 	// Runtime buffers to hold paths
 	char ad_project_root       [256];
@@ -40,25 +35,58 @@ namespace paths {
 	void ad_config(const char* config, char* buf, int32 n) { snprintf(buf, n, __ad_config, config); }
 };
 
-void init_paths() {
-	// Get the project root
-	GetModuleFileNameA(NULL, paths::ad_project_root, 256);
-	uint32 len = (uint32)strlen(paths::ad_project_root);
-
+void convert_executable_path_to_project_root(char* path, uint32 len) {
 	// Normalize
 	for (uint32 i = 0; i < len; i++) {
-		if (paths::ad_project_root[i] == '\\') paths::ad_project_root[i] = '/';
+		if (path[i] == '\\') path[i] = '/';
 	}
 
 	// Remove executable name, 'bin', and 'build'
 	int32 removed = 0;
 	for (uint32 i = len - 1; i > 0; i--) {
-		if (paths::ad_project_root[i] == '/') removed++;
-		paths::ad_project_root[i] = 0;
+		if (path[i] == '/') removed++;
+		path[i] = 0;
 		if (removed == 3) break;
 	}
+}
 
-	// Build the other paths from the project root
+void fill_path_templates();
+
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+
+#define _ad_executable_ext "bum"
+
+void init_paths() {
+	uint32 buffer_size = 256;
+	_NSGetExecutablePath(paths::ad_project_root, &buffer_size);
+	convert_executable_path_to_project_root(paths::ad_project_root, buffer_size);
+	printf("got project root, root = %s\n", paths::ad_project_root);
+	fill_path_templates();
+}
+#endif
+
+
+#ifdef _WIN32
+#include <windows.h>
+
+#define _ad_executable_ext ".exe"
+
+void init_paths() {
+	// Get the project root
+	GetModuleFileNameA(NULL, paths::ad_project_root, 256);
+	uint32 len = (uint32)strlen(paths::ad_project_root);
+
+	convert_executable_path_to_project_root(paths::ad_project_root, len);
+
+	fill_path_templates();
+}
+
+#endif
+
+
+// This has to be after everything is defined...?
+void fill_path_templates() {
 	snprintf(paths::ad_build,              256, _ad_build,              paths::ad_project_root);
 	snprintf(paths::ad_configs,            256, _ad_configs,            paths::ad_project_root);
 	snprintf(paths::__ad_config,           256, _ad_config,             paths::ad_project_root);
@@ -69,5 +97,3 @@ void init_paths() {
 	snprintf(paths::ad_feature_executable, 256, _ad_feature_executable, paths::ad_project_root);
 	snprintf(paths::ad_train_executable,   256, _ad_train_executable,   paths::ad_project_root);
 }
-
-#endif
